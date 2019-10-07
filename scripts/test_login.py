cbd9ad406f6c212b4b7609e82a228ab88c90445f
zhou <johndoe@example.com1818871431487@163.com>
2019-10-07 20:20:58 +0800

    初始化

---
+++ scripts/test_login.py
@@ -0,0 +1,11 @@
+class TestLogin:
+    def test_login01(self):
+        assert 1
+
+    def test_login02(self):
+        assert 0
+
+    def test_login03(self):
+        assert 0
+    def test_login04(self):
+        assert 1
